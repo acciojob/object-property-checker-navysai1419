6c6dbdf9b75dff3ba4a7894bc9780ea4d6faad17
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-26 14:49:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
+	return Object.prototype.hasOwnProperty.call(sampleObject, key);
   //   write your code here
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+const key = prompt("Enter Key."); 
 alert(hasKey(key));
